95a208bac984115dd4a8f4f79dcea1eb111c54e8
Jonathan D.A. Jewell <jonathan.jewell@open.ac.uk>
2026-02-22 22:18:03 +0000

    Auto-commit: Sync changes [2026-02-22]

--- wasm/target/wasm32-unknown-unknown/release/deps/i18n_wasm.d
+++
@@ -1,5 +0,0 @@
-/var/home/hyper/repos/polyglot-i18n/wasm/target/wasm32-unknown-unknown/release/deps/i18n_wasm.d: src/lib.rs
-
-/var/home/hyper/repos/polyglot-i18n/wasm/target/wasm32-unknown-unknown/release/deps/i18n_wasm.wasm: src/lib.rs
-
-src/lib.rs:
